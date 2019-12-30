e5c47e4762c7c77103a3a481771227eaf1d229ba
Aditya Shrivastava <adityashrivastava2799@gmail.com>
2019-12-30 23:46:12 +0530

    major change

--- script.js
+++ script.js
@@ -64,3 +64,19 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+
+//Array of images which you want to show: Use path you want.
+// var images=new Array('BBS_6611.jpg');
+// var nextimage=0;
+// doSlideshow();
+
+
+// function doSlideshow(){
+//     if(nextimage>=images.length){nextimage=0;}
+//     $('.global-header')
+//     .css('background-image','url("'+images[nextimage++]+'")')
+//     .fadeIn(500,function(){
+//         setTimeout(doSlideshow,1000);
+//     });
+// }
