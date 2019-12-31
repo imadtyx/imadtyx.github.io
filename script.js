34244fa443a137da51267df82e2dc78d49ce87f0
Aditya Shrivastava <adityashrivastava2799@gmail.com>
2020-01-01 00:00:07 +0530

    learnt animation

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// let previous = -1;
+// $(".icon[data-index]").click(function(){
+//     $(this).addClass("initialised");
+//     let index = $(this).attr("data-index");
+//     let navtab = $(this).closest("nav.tab").addClass("moving").attr("data-selected", index);
+//     if(previous == -1) navtab.find('.icon[data-index="2"]').addClass("initialised")
+//     if(previous == 1 && index == 3 || previous == 3 && index == 1) { //If going from one side to the other and middle needs to be hidden
+//         navtab.find('.icon[data-index="2"]').removeClass("initialised");
+//         setTimeout(function(){ //Because apparently this is the only way it will work
+//             navtab.find('.icon[data-index="2"]').addClass("initialised"); //Same animation as the other so they line up
+//         });
+//     }
+//     previous = index;
+//     setTimeout(function(){
+//         navtab.removeClass("moving").removeClass("hidemiddle");
+//     }, 750);
+// }); 
 var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
